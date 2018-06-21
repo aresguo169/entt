ffdf4d9b44306d0a52e7d23513dcf9614607ce6a
Michele Caini <michele.caini@gmail.com>
2018-06-21 08:35:27 +0200

    minor changes

--- src/entt/entity/prototype.hpp
+++ src/entt/entity/prototype.hpp
@@ -49,6 +49,11 @@ class Prototype final {
         basic_fn_type *assign;
     };
 
+    template<typename Component>
+    inline const Component & component() const ENTT_NOEXCEPT {
+        return registry->template get<Wrapper<Component>>(entity).component;
+    }
+
     void release() {
         if(registry->valid(entity)) {
             registry->destroy(entity);
@@ -184,8 +189,7 @@ public:
     template<typename... Component>
     decltype(auto) get() const ENTT_NOEXCEPT {
         if constexpr(sizeof...(Component) == 1) {
-            auto ident = [](const auto &component) -> decltype(auto) { return component; };
-            return (ident(registry->template get<Wrapper<Component>>(entity).component), ...);
+            return component<Component...>();
         } else {
             return std::tuple<const Component &...>{get<Component>()...};
         }
